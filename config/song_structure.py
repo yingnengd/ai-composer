8862244c1f231b0ed1f1c7421022ea7130254959
yingnengd <80525738+yingnengd@users.noreply.github.com>
2025-12-16 21:45:59 +0800

    Update song_structure.py

--- config/song_structure.py
+++ config/song_structure.py
@@ -1,8 +1,14 @@
 SONG_STRUCTURE = [
-    ("intro", 4, "low"),
-    ("verse", 8, "low"),
-    ("pre", 4, "mid"),
-    ("chorus", 8, "high"),
-    ("bridge", 4, "low"),
-    ("final", 8, "high")
+    ("intro",   8,  "low"),
+    ("verse",   16, "low"),
+    ("pre",     8,  "mid"),
+    ("chorus",  8,  "high"),
+
+    ("verse",   16, "low"),
+    ("pre",     8,  "mid"),
+    ("chorus",  8,  "high"),
+
+    ("bridge",  8,  "low"),
+    ("final",   12, "high"),
+    ("outro",   4,  "low")
 ]
